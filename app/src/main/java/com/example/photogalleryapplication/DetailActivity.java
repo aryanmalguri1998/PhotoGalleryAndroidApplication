b93493c0f72d7427aa0438e2da7fd4854e88a827
Aryan Malguri <abm7104@psu.edu>
2024-08-01 14:23:24 -0500

    Update DetailActivity.java

--- app/src/main/java/com/example/photogalleryapplication/DetailActivity.java
+++ app/src/main/java/com/example/photogalleryapplication/DetailActivity.java
@@ -1,15 +1,12 @@
 package com.example.photogalleryapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 import com.github.clans.fab.FloatingActionButton;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -20,17 +17,19 @@ import com.google.firebase.storage.StorageReference;
 
 public class DetailActivity extends AppCompatActivity {
 
+    // UI components
     TextView detailDesc, detailTitle, detailLang;
     ImageView detailImage;
     FloatingActionButton deleteButton, editButton;
-    String key = "";
-    String imageUrl = "";
+    String key = ""; // Key to identify the item in the database
+    String imageUrl = ""; // URL of the image to be displayed
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        // Initialize UI components
         detailDesc = findViewById(R.id.detailDesc);
         detailImage = findViewById(R.id.detailImage);
         detailTitle = findViewById(R.id.detailTitle);
@@ -38,6 +37,7 @@ public class DetailActivity extends AppCompatActivity {
         editButton = findViewById(R.id.editButton);
         detailLang = findViewById(R.id.detailLang);
 
+        // Retrieve data from the Intent extras
         Bundle bundle = getIntent().getExtras();
         if (bundle != null){
             detailDesc.setText(bundle.getString("Description"));
@@ -45,29 +45,40 @@ public class DetailActivity extends AppCompatActivity {
             detailLang.setText(bundle.getString("Language"));
             key = bundle.getString("Key");
             imageUrl = bundle.getString("Image");
-            Glide.with(this).load(bundle.getString("Image")).into(detailImage);
+
+            // Load the image using Glide
+            Glide.with(this).load(imageUrl).into(detailImage);
         }
+
+        // Set up click listener for the delete button
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Android Uploads");
                 FirebaseStorage storage = FirebaseStorage.getInstance();
 
+                // Reference to the image in Firebase Storage
                 StorageReference storageReference = storage.getReferenceFromUrl(imageUrl);
+                // Delete the image from storage
                 storageReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
+                        // Remove the image data from the Firebase Database
                         reference.child(key).removeValue();
                         Toast.makeText(DetailActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                        // Return to MainActivity
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         finish();
                     }
                 });
             }
         });
+
+        // Set up click listener for the edit button
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Start UpdateActivity with current data as extras
                 Intent intent = new Intent(DetailActivity.this, UpdateActivity.class)
                         .putExtra("Title", detailTitle.getText().toString())
                         .putExtra("Description", detailDesc.getText().toString())
