9bc8b0b830c53a0c9f5b390cf127069e783e13fc
Aryan Malguri <abm7104@psu.edu>
2024-08-01 14:22:12 -0500

    Update AboutFragment.java

--- app/src/main/java/com/example/photogalleryapplication/AboutFragment.java
+++ app/src/main/java/com/example/photogalleryapplication/AboutFragment.java
@@ -1,20 +1,22 @@
 package com.example.photogalleryapplication;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
+/**
+ * A simple {@link Fragment} subclass that displays information about the app.
+ */
 public class AboutFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        // This method is called to create the view hierarchy associated with the fragment.
+        // The layout file fragment_about.xml is inflated and returned as the view for this fragment.
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
